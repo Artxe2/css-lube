b0c38df8b36b91c0efa7fb7fb19c297b3d343a2a
artxe <balgoon24@gmail.com>
2022-08-07 20:43:51 +0900

    5412196``

--- src/components/atoms/sections/PaperAndToolsImgSection.svelte
+++ src/components/atoms/sections/PaperAndToolsImgSection.svelte
@@ -1,3 +1,3 @@
-<div class="backgroundImage=linearGradient(#ffffff40,#ffffff80),conicGradient(from_45deg_at_67%_67%,#3b599880,#3b599820),url(/pexels-karolina-grabowska-5412196.jpg) bg=white @dark@backgroundImage=conicGradient(from_-45deg_at_33%_67%,#3b5998,#3b599850),url(https://cdn.jsdelivr.net/gh/artxe2/instant-css@main/static/pexels-karolina-grabowska-5412196.jpg);c=white backgroundSize=cover">
+<div class="backgroundImage=linearGradient(#ffffff40,#ffffff80),conicGradient(from_45deg_at_67%_67%,#3b599880,#3b599820),url(/pexels-karolina-grabowska-5412196.jpg) bg=white @dark@backgroundImage=conicGradient(from_-45deg_at_33%_67%,#3b5998,#3b599850),url(/pexels-karolina-grabowska-5412196.jpg);c=white backgroundSize=cover">
     <slot></slot>
 </div>
